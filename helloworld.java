56391195a1a4b16ff3f2e437984372bda25cb536
JuniorTemp <junior_sadaanzinhoa@hotmail.com>
2023-11-10 10:33:43 -0300

    first commit

---
+++ helloworld.java
@@ -0,0 +1,21 @@
+public class helloworld {
+
+    public static void main (String[] args ){
+        
+        String ambient = args [0];
+        if (ambient.equalsIgnoreCase("Junior")){
+           System.out.print("Junior ama a Gi");
+        } else if (ambient.equalsIgnoreCase("Geovanna")){
+           System.out.print("Geovanna ama Junior querido");
+        }else {
+              } System.out.print(ambient);
+
+        
+        
+
+
+
+    
+    
+    }
+}
